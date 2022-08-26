e0f3d2fcf2c4ba3a51b10930d51e9546282fbae6
Ryan <santoryan1408@gmail.com>
2022-08-26 18:03:37 -0300

    new vite config

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    port: 3000    
-},
+  base: '/weather--app/',
+  plugins: [react()]
 })
